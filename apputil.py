af13b627fcc7343cad116322c5a1d957c5d35ccc
Lauren T <ltolley@iu.edu>
2025-09-12 16:23:43 +0000

    completing fibonacci function

--- apputil.py
+++ apputil.py
@@ -9,6 +9,8 @@ def fibonacci(n):
         print("Input should be a positive integer.")
         return None
     elif n == 1:
-        print (0)
-    elif n >= 2: 
-        return 
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
