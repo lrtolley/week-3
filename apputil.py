e191f013aed19409c4726b412717f8fc21fbb350
Lauren T <ltolley@iu.edu>
2025-09-12 16:07:34 +0000

    starting defining fibonacci function

--- apputil.py
+++ apputil.py
@@ -1,5 +1,14 @@
 import seaborn as sns
 import pandas as pd
-
+import numpy as np
 
 # update/add code below ...
+def fibonacci(n):
+    '''Return the nth Fibonacci number.'''
+    if n <= 0:
+        print("Input should be a positive integer.")
+        return None
+    elif n == 1:
+        print (0)
+    elif n >= 2: 
+        return 
