d7adbd8cff7ba3b070b6b652dd2a8f8f3c70393e
Lauren T <ltolley@iu.edu>
2025-10-24 20:58:02 +0000

    binary code fix

--- apputil.py
+++ apputil.py
@@ -16,21 +16,16 @@ def fibonacci(number):
 
 
 def to_binary(number):
-    '''Converts a positive integer base 10 number to a binary number'''
-    if number == 0: #defines 0 in binary
+    '''Convert a positive integer to binary using recursion.'''
+    if n < 0:
+        return None
+    elif n == 0:
         return "0"
-    if number < 0: #prevents negative input 
-      print("Input should be a positive integer.")
-    return None
-    binary_digits = [] #starts an empty list to hold the binary digits
-    dividend = number
-    while dividend > 0:
-        quotient, remainder = divmod(dividend, 2) #divides the number by 2 and gets the 
-        #quotient and remainder
-        binary_digits.insert(0, str(remainder)) #inserts the remainder in the frontmost 
-        #position of the list
-        dividend = quotient #updates the dividend to be the quotient for the next iteration
-    return "".join(binary_digits) #joins the list of binary digits into a string and returns it
+    elif n == 1:
+        return "1"
+    else:
+        return to_binary(n // 2) + str(n % 2)
+
 
 url = 'https://github.com/melaniewalsh/Intro-Cultural-Analytics/raw/master/book/data/bellevue_almshouse_modified.csv'
 df_bellevue = pd.read_csv(url) #reads in the csv file from the url
